b3d7350c10b7828c943e5d2b64d9fa16c3ac976b
DecireeSierra <deciree@gurucx.com>
2023-03-08 09:08:46 -0500

    se crea desarrollo para el Tipo de Documento--Deciree

--- fa_backend/fielappbackend/fielappbackend/urls.py
+++ fa_backend/fielappbackend/fielappbackend/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path as url, include
+
+#urlpatterns = [
+ #   path('admin/', admin.site.urls),
+#]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^', include('Tipo_Documento.urls')),
+    #url('api/', include(router_posts.urls)),
+    #path('api/instances/', post.api.views.InstanceList.as_view(), name="instances"),
 ]
