df33dd1bed62aec6d94102d61a4abc7a994d5db3
Sandra Hernandez <sandra@gurucx.com>
2023-03-13 10:36:44 -0500

    Arreglo de Backend cambio de gitignore para subir la env

--- fa_backend/fielappbackend/fielappbackend/urls.py
+++ fa_backend/fielappbackend/fielappbackend/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import re_path as url, include
 
-#urlpatterns = [
- #   path('admin/', admin.site.urls),
-#]
-
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'^', include('Tipo_Documento.urls')),
