54e3c355c50eacb40c0885d659a13639fbe25d79
Sandra Hernandez <sandra@gurucx.com>
2023-04-04 09:43:55 -0500

    Cambios en Tipo_documento

--- fa_backend/fielappbackend/Tipo_Documento/models.py
+++ fa_backend/fielappbackend/Tipo_Documento/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Tipo_Documento(models.Model):
-    Nom_documento = models.CharField(max_length=70, blank=False, default='')
-    Des_documento = models.CharField(max_length=200,blank=False, default='')
+    Nom_documento = models.CharField(max_length=2, help_text="Introduzca las siglas del Tipo de Documento (Max 2 caracteres)", blank=False, default='',verbose_name='Nomenclatura Tipo Documento')
+    Des_documento = models.CharField(max_length=50,help_text="Introduza la descripción del Tipo de Documento", blank=False, default='',verbose_name='Descripción Tipo Documento')
 # Create your models here.
