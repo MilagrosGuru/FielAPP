39fdbe8184734902f3b6ba2bcc2687cebe4be7c6
DecireeSierra <deciree@gurucx.com>
2023-10-20 12:04:54 -0500

    se crea funcion para eliminar premio fiel

--- fa_backend/fielappbackend/Reward/models.py
+++ fa_backend/fielappbackend/Reward/models.py
@@ -1,19 +1,19 @@
 from djongo import models
-from datetime import date
+from storages.backends.s3boto3 import S3Boto3Storage
+from datetime import datetime
 
 
 class Reward(models.Model):
     activate= models.BooleanField(default=False, blank=True)
-    '''URL = models.URLField(max_length=200, blank=True, default='')'''
-    URL = models.FileField(upload_to='uploads/')  # Campo FileField
+    URL = models.FileField(upload_to='uploads/', storage=S3Boto3Storage())  # Campo FileField
     minimun_cost = models.FloatField(max_length=100, blank=True, default='')
     description= models.CharField(max_length=600, blank=True, default='')
     redeem_points= models.IntegerField(blank=True, default='')
     datetime_activate = models.DateField(max_length=50, blank=True, default='1900-11-11')
     has_expiration_date = models.BooleanField(default=False, blank=True)
     datetime_expiration = models.DateField(max_length=50, blank=True, default='1900-11-11')
-    create_datetime= models.DateField(max_length=50, blank=True, default=date.today)
-    update_datetime= models.DateField(max_length=50, blank=True, default=date.today) #si es la primera vez es el mismo create_datetime
+    create_datetime= models.DateField(max_length=50, blank=True, default=datetime.now())
+    update_datetime= models.DateField(max_length=50, blank=True, default=datetime.now()) #si es la primera vez es el mismo create_datetime
     update_user= models.IntegerField(blank=True, default='') #quien lo actualizo
     create_user = models.IntegerField(blank=True, default='') #quien creo el premio
     company = models.JSONField(null=True, blank=True) #empresa
--- fa_backend/fielappbackend/Reward/views.py
+++ fa_backend/fielappbackend/Reward/views.py
@@ -13,31 +13,26 @@ from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from rest_framework import generics
 import json
+from datetime import datetime
 
 client = MongoClient(settings.MONGODB_HOST)
 db = client[settings.MONGODB_DB]
 
 
-class RewardAPIView(APIView):   #otra forma de crear compañia
-    def post(self, request, format=None):
+class RewardAPIView(APIView):   #otra forma de crear premio
+    def post(self, request):
         #URL = request.FILES['URL']
-        data = request.body
-        #datos_json = request.POST['datos']
-        data=request.data
+        data = request.data
         data['company']=json.loads(request.POST['company'])
-        serializer = RewardSerializer(data=request.data)
+        serializer = RewardSerializer(data=data)
 
         try:
-            if serializer.is_valid():
-                print("4")
-                
+            if serializer.is_valid():  
                 reward = serializer.save()  # Guarda el objeto en la base de datos
-                print("1")
                 reward_id = reward.id  # Accede al ID del objeto creado
-                print(reward_id)
                 return Response({"message": "Premio creado", "reward_id": reward_id}, status=status.HTTP_201_CREATED)
-            
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             print("Error:", str(e))
 
@@ -81,3 +76,30 @@ class RewardAPIView(APIView):   #otra forma de crear compañia
             
         except Exception as e:
             print("Error:", str(e))
+
+    @api_view(['DELETE']) #funcion para eliminar premio fiel, recibe id de premio y id de usuario que realiza la eliminacion
+    def delete_reward(request, id, id_usuario):
+    # Connect to the MongoDB database
+        try:
+            collection = db.Reward_reward
+            #user = collection.find_one({'id': int(user_id)})
+            if request.method=='DELETE':
+                # Get the user document with the provided ID
+                #user = collection.find_one({'_id': user_id})
+                    # Update the user document with the provided data
+                    data = collection.find_one({'id': int(id)})  
+                    date_update = datetime.now()
+                    collection.update_one({'id': int(data['id'])}, {'$set': 
+                        {
+                        'activate': False,
+                        'update_datetime': date_update,
+                        'update_user':id_usuario
+                        }})
+
+                    # Return a success response
+                    return JsonResponse({"message": "Premio eliminado", "id": data['id']}, status=status.HTTP_200_OK) 
+            
+        except Exception as e:
+            print("Error:", str(e))
+
+
